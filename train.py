5ff62212d9e2282494e24a2497d3cd32eba9c665
brian-w-projects <brian.weinfeld@gmail.com>
2018-07-05 16:02:49 -0400

    updated files

--- train.py
+++ train.py
@@ -1,10 +1,23 @@
 from __future__ import division
 import nltk, re, pprint
+from nltk.corpus import stopwords
 import urllib.request
 from bs4 import BeautifulSoup
 
-with urllib.request.urlopen('http://news.bbc.co.uk/2/hi/health/2284783.stm') as response:
-    html = response.read()
-    raw = BeautifulSoup(html, 'html.parser')
-    tokens = nltk.word_tokenize(raw.get_text())
-    print(tokens[:20])
+def feature(i):
+    with urllib.request.urlopen(f'https://raw.githubusercontent.com/brian-cuny/620week5assignment2/master/Train/Bands/{i}') as response:
+        html = response.read()
+        raw = BeautifulSoup(html, 'html.parser')
+        tokens = nltk.word_tokenize(raw.get_text())
+        tokens = set(w.lower() for w in tokens if w.isalpha()) #only words
+        tokens = tokens.difference(stopwords.words('english')) #keep import words
+
+        features = {}
+        for t in tokens:
+            features[t] = True
+        return features
+
+featuresets = [(feature(i), 'Bands') for i in range(1,5)]
+
+
+print(featuresets)
