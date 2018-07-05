8ae78fcd1862fdd6a08cbb1cba2c3a171128a477
brian-w-projects <brian.weinfeld@gmail.com>
2018-07-05 16:51:12 -0400

    draft

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
-from __future__ import division
-import nltk, re, pprint
+import nltk
 from nltk.corpus import stopwords
-import urllib.request
 from bs4 import BeautifulSoup
+import random
+import pickle
 
-def feature(i):
-    with urllib.request.urlopen(f'https://raw.githubusercontent.com/brian-cuny/620week5assignment2/master/Train/Bands/{i}') as response:
+def feature(topic, i):
+    with open(f'/home/brian/Desktop/DataScience/620/620week5assignment2/Train/{topic}/{i}', 'rb') as response:
         html = response.read()
         raw = BeautifulSoup(html, 'html.parser')
         tokens = nltk.word_tokenize(raw.get_text())
@@ -17,7 +17,32 @@ def feature(i):
             features[t] = True
         return features
 
-featuresets = [(feature(i), 'Bands') for i in range(1,5)]
+def initial_read():
+    featuresets_bands = [(feature('Bands', i), 'Bands') for i in range(1,61)]
+    featuresets_bio = [(feature('BioMedical', i), 'BioMedical') for i in range(1,61)]
+    featuresets_goats = [(feature('Goats', i), 'Goats') for i in range(1,58)]
+    featuresets_sheeps = [(feature('Sheep', i), 'Sheep') for i in range(1,57)]
 
+    full_set = featuresets_bands + featuresets_bio + featuresets_goats + featuresets_sheeps
+    random.shuffle(full_set)
 
-print(featuresets)
+    file_name = 'data'
+    file_object = open(file_name, 'wb')
+    pickle.dump(full_set, file_object)
+    file_object.close()
+
+# initial_read()
+file_name = 'data'
+file_object = open(file_name, 'rb')
+full_set = pickle.load(file_object)
+
+random.shuffle(full_set)
+size = int(len(full_set) * 0.1)
+
+train_set, test_set = full_set[size:], full_set[:size]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print(nltk.classify.accuracy(classifier, test_set))
+classifier.show_most_informative_features(5)
+
+tree_classifier = nltk.classify.DecisionTreeClassifier.train(train_set)
+print(nltk.classify.accuracy(tree_classifier, test_set))
